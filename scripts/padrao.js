5ddfadd7d5d43090f8ad9188482857b8bc6fcc73
Lucas Borges <114541600+Balcaremen@users.noreply.github.com>
2026-05-07 16:23:51 -0300

    Remoção dos scripts js
    Descubri que tinha como separar os scripts js do html, mas por enquanto não é permitido esse uso

--- scripts/padrao.js
+++
@@ -1,7 +0,0 @@
-{
-    // Mudando o valor padrão de Data de Nascimento para a data atual
-    document.getElementById("data").valueAsDate = new Date();
-
-    // Mudando o valor padrão de Idioma para o idioma do navegador
-    document.getElementById("idioma").value = navigator.language;
-}
